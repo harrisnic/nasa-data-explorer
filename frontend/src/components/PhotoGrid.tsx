3655a7a5d5ad2604830bf1b83087272d39beb01e
harrisnic <harris.nicolaou@gmail.com>
2025-07-12 17:26:39 +0300

    Refactor, preselects first rover and gets latest photos

--- frontend/src/components/PhotoGrid.tsx
+++ frontend/src/components/PhotoGrid.tsx
@@ -4,13 +4,23 @@ import PhotoCard from "./PhotoCard.tsx";
 import PhotoCardSkeleton from "@/components/PhotoCardSkeleton.tsx";
 import PhotoCardContainer from "@/components/PhotoCardContainer.tsx";
 import AlertBox from "@/components/AlertBox.tsx";
+import type {Rover} from "@/types";
 
-const PhotoGrid = () => {
+interface Props {
+    selectedRover: Rover | null;
+}
 
-    const {data, error, loading} = usePhotos()
+const PhotoGrid = ({selectedRover}: Props) => {
+
+    if (!selectedRover) return null; // Guard clause
+
+    console.log(selectedRover)
+
+    const {data, error, loading} = usePhotos(selectedRover)
     const skeletons = [1, 2, 3, 4, 5, 6]
 
     if (error) return <AlertBox status="error" description={error} />
+
 
     return (
         <SimpleGrid
@@ -30,7 +40,6 @@ const PhotoGrid = () => {
                     </PhotoCardContainer>
                 ))
             }
-
         </SimpleGrid>
     )
 }
