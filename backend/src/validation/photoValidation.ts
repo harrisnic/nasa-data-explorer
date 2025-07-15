742d596516e643d5583921597e8da09acfc8f3ff
harrisnic <harris.nicolaou@gmail.com>
2025-07-15 16:16:29 +0300

    Updating axios config and other fixes

--- backend/src/validation/photoValidation.ts
+++ backend/src/validation/photoValidation.ts
@@ -5,8 +5,8 @@ export const photoValidationRules = {
         query('rover').isString().withMessage('Rover name required'),
         query('date')
             .optional()
-            .matches(/^\d{4}-([1-9]|1[0-2])-([1-9]|[12]\d|3[01])$/)
-            .withMessage('Date must be in YYYY-M-D format (no leading zeros)')
+            .matches(/^\d{4}-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|3[01])$/)
+            .withMessage('Date must be in YYYY-MM-DD format')
     ],
     getByLatestDate: [
         param('rover').isString().withMessage('Rover name is required'),
