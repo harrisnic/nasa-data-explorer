dead9ae05a8aac5f328276427065a3a26323d994
harrisnic <harris.nicolaou@gmail.com>
2025-07-18 21:52:37 +0300

    Hide/Show menus om breakpoints

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,10 +1,15 @@
 import Header from "@/components/Header.tsx";
 import {Outlet} from "react-router-dom";
+import RoverList from "@/components/RoverList.tsx";
+import {Box} from "@chakra-ui/react";
 
 const Layout = () => {
     return (
         <>
             <Header/>
+            <Box display={{ base: "block", lg: "none" }} m="6">
+                <RoverList/>
+            </Box>
             <Outlet/>
         </>
     )
