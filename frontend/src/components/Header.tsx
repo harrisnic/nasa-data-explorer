a5761a5ddd3a7c132c4a94dba24c772635b1b08d
harrisnic <harris.nicolaou@gmail.com>
2025-07-19 16:43:02 +0300

    Move mob rover list comp to navbar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,16 +3,20 @@ import NasaLogo from '../assets/nasa-logo.svg'
 import {ColorModeButton} from "@/components/ui/color-mode.tsx";
 import NavBar from "@/components/NavBar.tsx";
 import {Link} from "react-router-dom";
+import RoverList from "@/components/RoverList.tsx";
 
 const Header = () => {
     return (
         <HStack justifyContent="space-between" padding="6" mb="8">
             <HStack >
                 <Link to="/">
-                    <Image me="12" boxSize="60px" src={NasaLogo} alt="NASA logo" />
+                    <Image me={{ base: "3", lg: "12" }} boxSize="60px" src={NasaLogo} alt="NASA logo" />
                 </Link>
                 <Box display={{ base: "none", lg: "block" }}>
                     <NavBar/>
+                </Box>
+                <Box display={{ base: "block", lg: "none" }}>
+                    <RoverList/>
                 </Box>
             </HStack>
             <ColorModeButton/>
--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,16 +1,11 @@
 import Header from "@/components/Header.tsx";
 import {Outlet} from "react-router-dom";
-import RoverList from "@/components/RoverList.tsx";
-import {Box} from "@chakra-ui/react";
 import BackToTopButton from "@/components/BackToTopButton.tsx";
 
 const Layout = () => {
     return (
         <>
             <Header/>
-            <Box display={{ base: "block", lg: "none" }} ml="6" mb="6">
-                <RoverList/>
-            </Box>
             <Outlet/>
             <BackToTopButton/>
         </>
