7e4c64823340b76d14aec8c71cb3af7811635638
harrisnic <harris.nicolaou@gmail.com>
2025-07-20 00:01:01 +0300

    Nasa Bot page and components

---
+++ frontend/src/components/bot/NasaBotResponse.tsx
@@ -0,0 +1,20 @@
+import {Box, SkeletonText, Text} from "@chakra-ui/react";
+import type {BotResponse} from "@/types";
+import Error from "@/components/Error.tsx";
+
+interface NasaBotResponseProps {
+    data: BotResponse;
+    loading: boolean;
+    error?: string;
+}
+
+const NasaBotResponse = ({ data, loading, error }: NasaBotResponseProps) => {
+    if (error) return  <Error description={error} />
+    if (loading) return <SkeletonText noOfLines={4} />
+    return (
+        <Box p="3" borderRadius="md" h="220px" overflow="auto" bg="gray.100" _dark={{ bg: "gray.800" }}>
+            <Text>{data}</Text>
+        </Box>
+    );
+}
+export default NasaBotResponse
