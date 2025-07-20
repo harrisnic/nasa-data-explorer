c6c5c50c59b69eb7cd2f6e4974dea9bde2755774
harrisnic <harris.nicolaou@gmail.com>
2025-07-20 12:21:48 +0300

    VITE env variables

--- frontend/src/services/api-client.ts
+++ frontend/src/services/api-client.ts
@@ -8,7 +8,7 @@ export interface FetchResponse<T> {
 }
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 class APIClient<T> {
